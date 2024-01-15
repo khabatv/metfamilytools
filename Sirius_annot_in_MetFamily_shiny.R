0292b537f135c8c313bf753e74159d7663d58eae
khabatv <61308485+khabatv@users.noreply.github.com>
2024-01-15 08:30:14 +0100

    Update Sirius_annot_in_MetFamily_shiny.R

--- Sirius_annot_in_MetFamily_shiny.R
+++ Sirius_annot_in_MetFamily_shiny.R
@@ -8,6 +8,8 @@ options(shiny.maxRequestSize = 3000 * 1024^2)  # Set to 30 MB (adjust as needed)
 ui <- fluidPage(
   tags$div(
     style = "border: 1px solid #ccc; padding: 10px; margin-bottom: 20px;",
+    tags$h4("Annotation tool for MetFamily PrecursorMatrix"),
+      tags$li("you can add annotations from different sources using this tool, if they have identical alingment ID with Metfamily or MsDial data."),
     tags$h4("Instructions"),
     tags$ol(
       tags$li("Upload canopus_compound_summary.tsv  and metFamily's PrecursorMatrix files."),
@@ -22,7 +24,7 @@ ui <- fluidPage(
   # Buttons and numeric input
   actionButton("submit_part1", "Show canopus annotations "),
   verbatimTextOutput("headers"),  # Add this line to display the column headers
-  numericInput("column_number", "Column Number for uniqueAnnotions:", value = 8),
+  numericInput("column_number", "insert Column Number of the annotation:", value = 8),
   actionButton("submit_part2", "Creat PrecursorMatrix with canopus annotation"),
   downloadButton("download_part2", "Download PrecursorMatrix with canopus annotation"),
   textOutput("message")
