116435e78190be315fbbb0706489f2b95d86de3e
khabatv <61308485+khabatv@users.noreply.github.com>
2024-01-18 09:56:33 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,25 +1,39 @@
-# ui.R
-fluidPage(
-  tags$div(
-    style = "border: 1px solid #ccc; padding: 10px; margin-bottom: 20px;",
-    tags$h4("Annotation tool for MetFamily PrecursorMatrix"),
-    tags$li("you can add annotations from different sources using this tool, if they have identical alingment ID with Metfamily or MsDial data."),
-    tags$h4("Instructions"),
-    tags$ol(
-      tags$li("Upload canopus_compound_summary.tsv  and metFamily's PrecursorMatrix files."),
-      tags$li("Press 'Show canopus annotations'."),
-      tags$li("From the table, select the number of the annotation column and write it in the 'Column Number of Annotion' text box."),
-      tags$li("Press 'Creat PrecursorMatrix with canopus annotation'.")
+library(shiny)
+library(data.table)
+
+# Define the Shiny UI
+ui <- fluidPage(
+
+  # Application title
+  titlePanel("Annotation tool for MetFamily PrecursorMatrix"),
+
+  # Sidebar with inputs
+  sidebarLayout(
+    sidebarPanel(
+
+      # Select TSV file
+      fileInput("file", "Choose TSV File"),
+
+      # Select CSV file
+      fileInput("file2", "Choose CSV File", accept = c(".csv")),
+
+      # Show annotation
+      actionButton("submit_part1", "Show canopus annotations"),
+
+      # Column number of annotation
+      numericInput("column_number", "Column Number of Annotion:", value = 8),
+
+      # Create precursor matrix with annotation
+      actionButton("submit_part2", "Creat PrecursorMatrix with canopus annotation"),
+
+      # Download precursor matrix with annotation
+      downloadButton("download_part2", "Download PrecursorMatrix with canopus annotation")
+    ),
+
+    # Main panel with output
+    mainPanel(
+      uiOutput("headers"),
+      textOutput("message")
     )
-  ),
-  # File inputs
-  fileInput("file", "Choose TSV File"),
-  fileInput("file2", "Choose CSV File", accept = c(".csv")),
-  # Buttons and numeric input
-  actionButton("submit_part1", "Show canopus annotations "),
-  verbatimTextOutput("headers"),  # Add this line to display the column headers
-  numericInput("column_number", "insert Column Number of the annotation:", value = 8),
-  actionButton("submit_part2", "Creat PrecursorMatrix with canopus annotation"),
-  downloadButton("download_part2", "Download PrecursorMatrix with canopus annotation"),
-  textOutput("message")
+  )
 )
